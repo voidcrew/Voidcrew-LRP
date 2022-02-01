0a694142df1d9a85245f09040015926ef21ad6a8
John Willard <53777086+JohnFulpWillard@users.noreply.github.com>
2022-02-01 02:29:52 -0500

    Revert "Gets things ready for voidcrew dev (#85)"
    This reverts commit ccc8cb2a618658515d055aae86c358926a3dec4d.

--- tools/build/build.js
+++ tools/build/build.js
@@ -22,7 +22,7 @@ Juke.setup({ file: import.meta.url }).then((code) => {
   process.exit(code);
 });
 
-const DME_NAME = 'voidcrew';
+const DME_NAME = 'tgstation';
 
 export const DefineParameter = new Juke.Parameter({
   type: 'string[]',
